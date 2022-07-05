37669f662c0747400c6d76dc997a57d90e366bb1
PedroHombre <radekgornackiv2@gmail.com>
2022-07-05 15:12:04 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+document.getElementById('submit').addEventListener('click', calculate);
+let output = document.getElementById('result');
+let inputF = document.getElementById('inputDefaultSize');
+let inputS = document.getElementById('inputSize');
+
+function calculate() {
+	let valueFirst = inputF.value;
+	let valueSecond = inputS.value;
+
+	valueFirst = valueFirst.trim();
+	valueSecond = valueSecond.trim();
+
+	valueFirst = parseInt(valueFirst);
+	valueSecond = parseInt(valueSecond);
+
+	if (!Number.isInteger(valueFirst)) {
+		inputF.classList.add('invalid');
+
+		if (!Number.isInteger(valueSecond)) {
+			inputS.classList.add('invalid');
+		}
+		return;
+	}
+
+	console.log(valueFirst);
+	console.log(valueSecond);
+
+	let result = valueSecond / valueFirst;
+
+	if (Number.isNaN(result)) result = 0;
+
+	output.innerHTML = result;
+}
