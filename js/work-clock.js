224fbca5d4d6c7a20a1d9647e80d61d128366b5d
PedroHombre <radekgornackiv2@gmail.com>
2022-07-07 10:02:38 +0200

    work countdown clock alpha version

--- js/work-clock.js
+++ js/work-clock.js
@@ -5,14 +5,24 @@
 
 	function countdown() {
 		let now = new Date();
-		if (now > finishHour) finishHour.setDate(finishHour.getDate() + 1);
+		let timeoutComplete = false;
+
+		now > finishHour ? (timeoutComplete = true) : (timeoutComplete = false);
+
+		if (timeoutComplete === true) {
+			clock.classList.add('complete');
+			clock.innerText = 'fajrant';
+
+			setTimeout(countdown, 1000);
+			return;
+		}
 
 		let remaining = (finishHour - now) / 1000;
 		let hours = Math.floor((remaining / 60 / 60) % 60);
 		let minutes = Math.floor((remaining / 60) % 60);
 		let seconds = Math.floor(remaining % 60);
 
-		clock.innerHTML = hours + ' : ' + minutes + ' : ' + seconds;
+		clock.innerText = hours + ' : ' + minutes + ' : ' + seconds;
 		setTimeout(countdown, 1000);
 	}
 
